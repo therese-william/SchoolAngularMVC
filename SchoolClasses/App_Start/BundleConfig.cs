830b78308277dd3755438a0c5ccf8385977ca8f9
Therese Missiha <theresewilliam@gmail.com>
2016-08-22 06:49:04 +1000

    css

--- SchoolClasses/App_Start/BundleConfig.cs
+++ SchoolClasses/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace SchoolClasses
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.min.css"));
         }
     }
 }
